323137841066c2ec72dbd08a12cdb8153fcb005d
alex3144 <alexboceno@hotmail.fr>
2017-08-22 11:34:05 +0200

    config

--- models/index.js
+++ models/index.js
@@ -4,10 +4,7 @@ var authentication = require('./auth_model');
 var user = require('./user_model');
 var workspace = require('./workspace_model')
 var workspaceComponent = require('./workspace_component_model')
-<<<<<<< HEAD
 var cache = require('./cache_model')
-=======
->>>>>>> 8eff1e2a3176eb1df350f8bf10892cb4e6e4c530
 
 // --------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------
@@ -17,10 +14,6 @@ module.exports = {
     authentication: authentication,
     user: user,
     workspace: workspace,
-<<<<<<< HEAD
     workspaceComponent: workspaceComponent,
     cache: cache
-=======
-    workspaceComponent: workspaceComponent
->>>>>>> 8eff1e2a3176eb1df350f8bf10892cb4e6e4c530
 }
