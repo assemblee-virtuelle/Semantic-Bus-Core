cebf22f50cd8e3201090c86b6e44d482215366a6
alex3144 <alexboceno@hotmail.fr>
2017-08-22 17:06:18 +0200

    fix bug

--- models/mangopay_auth_model.js
+++ models/mangopay_auth_model.js
@@ -1,6 +1,6 @@
 'use strict';
 
 var mongoose = require('../db/mongo_client');
-var Mangopayauthschema = require('../model_schemas/mangoPayAuthSchema');
+var Mangopayauthschema = require('../model_schemas').mangoPayAuthSchema;
 
 module.exports = mongoose.model('workspaceComponent', Mangopayauthschema);
--- models/transaction_model.js
+++ models/transaction_model.js
@@ -1,6 +1,6 @@
 'use strict';
 
 var mongoose = require('../db/mongo_client');
-var transactionSchema = require('../model_schemas/transaction');
+var transactionSchema = require('../model_schemas').transaction;
 
 module.exports = mongoose.model('transaction', transactionSchema);
