87c6bac428e55ab60a673b47e06ffb8bf9683ffe
alex3144 <alexboceno@hotmail.fr>
2017-08-22 18:09:17 +0200

    fix bug

--- models/workspace_model.js
+++ models/workspace_model.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var mongoose = require('../db/mongo_client');
-var WorkspaceSchema = require('../model_schemas').workspace;
+var WorkspaceSchema = require('../model_schemas/workspace_component_schema');
 // var WorkspaceSchema = require('../model_schemas/workspace').workspace;
 
 module.exports = mongoose.model('workspaces', WorkspaceSchema);
