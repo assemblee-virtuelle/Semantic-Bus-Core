46a4846bef9b92b33e66d937853267a98eff434a
alex3144 <alexboceno@hotmail.fr>
2017-08-22 16:30:40 +0200

    fbug

--- models/cache_model.js
+++ models/cache_model.js
@@ -1,6 +1,7 @@
 'use strict';
 console.log(__filename);
 var mongoose = require('../db/mongo_client');
-var CacheSchema = require('../model_schemas.js').cache;
+var CacheSchema = require('../model_schemas/cache');
+// var CacheSchema = require('../model_schemas').cache;
 
 module.exports = mongoose.model('cache', CacheSchema);
