9a14b1c85dd5b7ac53758c056f7b5d5eaec1cd38
alex3144 <alexboceno@hotmail.fr>
2017-08-22 15:54:46 +0200

    dbg

--- models/user_model.js
+++ models/user_model.js
@@ -1,6 +1,6 @@
 'use strict';
 
 var mongoose = require('../db/mongo_client');
-var userSchema = require('../model_schemas').user;
+// var userSchema = require('../model_schemas').user;
 
 module.exports = mongoose.model('user', userSchema);
