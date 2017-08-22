54a1b4bdaadf7c176348ef09c19ad087d21e4beb
alex3144 <alexboceno@hotmail.fr>
2017-08-22 16:03:49 +0200

    test

--- models/cache_model.js
+++ models/cache_model.js
@@ -1,6 +1,6 @@
 'use strict';
 console.log(__filename);
 var mongoose = require('../db/mongo_client');
-// var CacheSchema = require('../model_schemas.js').cache;
+var CacheSchema = require('../model_schemas.js').cache;
 
 module.exports = mongoose.model('cache', CacheSchema);
--- models/workspace_component_model.js
+++ models/workspace_component_model.js
@@ -1,6 +1,6 @@
 'use strict';
 
 var mongoose = require('../db/mongo_client');
-// var WorkspaceComponentSchema = require('../model_schemas').workspaceComponent;
+var WorkspaceComponentSchema = require('../model_schemas').workspaceComponent;
 
 module.exports = mongoose.model('workspaceComponent', WorkspaceComponentSchema);
