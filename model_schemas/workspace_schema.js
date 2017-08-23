7870c2ee6cf297406b1b961bd82f7a0279a1d6c2
alex3144 <alexboceno@hotmail.fr>
2017-08-23 17:21:36 +0200

    debug ..

--- model_schemas/workspace_schema.js
+++ model_schemas/workspace_schema.js
@@ -1,7 +1,6 @@
 'use strict';
-
 var mongoose = require('mongoose');
-var workspaceComponent = require('./workspace_component_schema')
+// var workspaceComponent = require('./workspace_component_schema')
 
 // --------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------
@@ -11,7 +10,7 @@ var WorkspaceSchema = mongoose.Schema({
     name: String,
     description: String,
     components: [{
-        type: mongoose.Schema.Types.ObjectId, 
+        type: mongoose.Schema.Types.ObjectId,
         ref: "workspaceComponent"
     }],
     dates: {
@@ -23,7 +22,7 @@ var WorkspaceSchema = mongoose.Schema({
     },
     average_consumption: {
         type: Number,
-        default: 0 
+        default: 0
     }
 });
 
--- models/workspace_model.js
+++ models/workspace_model.js
@@ -1,6 +1,5 @@
 'use strict';
-
 var mongoose = require('../db/mongo_client');
-var WorkspaceSchema = require('../model_schemas/workspace_component_schema');
+var WorkspaceSchema = require('../model_schemas/workspace_schema');
 
-module.exports = mongoose.model('workspace', WorkspaceSchema, "workspaces");
+module.exports = mongoose.model('workspace', WorkspaceSchema);
