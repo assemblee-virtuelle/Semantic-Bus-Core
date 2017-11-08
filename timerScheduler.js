6ebd7d60d69a8a0cde2a2c6978c5e81c9124f8e0
simon louvet <simon.louvet.zen@gmail.com>
2017-11-08 19:58:49 +0100

    timer work in dedicated server and not

--- timerScheduler.js
+++ timerScheduler.js
@@ -4,89 +4,99 @@ module.exports = {
   jwt: require('jwt-simple'),
   moment: require('moment'),
   http: require('http'),
-  url : require('url'),
-  config : require('../../configuration.js'),
+  url: require('url'),
+  config: require('../../configuration.js'),
   runTimers: function(dedicaded) {
 
     this.componentLib.get_all({
       module: 'timer'
     }).then(components => {
-      console.log(components.length + ' timers');
-      components.forEach(c => {
+      console.log('ALLO',dedicaded,this.config.timerTarget);
 
-        let now = new Date();
-        let lastExec = c.specificData.last == undefined ? undefined : new Date(c.specificData.last);
-        if (c.specificData.interval != undefined) {
-          let interval = c.specificData.interval;
-          console.log(now - (interval * 1000 * 60), lastExec);
-          if (lastExec == undefined || (interval != undefined && now - (interval * 1000 * 60) >= lastExec)) {
-            c.specificData.last = now;
-            //console.log(c);
-            this.componentLib.update(c);
-
-            if (dedicaded && this.config.timerTarget!=undefined) {
-
-              const payload = {
-                exp: this.moment().add(14, 'days').unix(),
-                iat: this.moment().unix(),
-                iss: 'timerScheduler'
-              }
-
-              const token = this.jwt.encode(payload, this.config.timerTarget.secret);
+      if ((dedicaded && this.config.timerTarget != undefined) || (dedicaded==undefined && this.config.timerTarget == undefined)) {
+        console.log(components.length + ' timers');
+        components.forEach(c => {
+          let now = new Date();
+          let lastExec = c.specificData.last == undefined ? undefined : new Date(c.specificData.last);
+          if (c.specificData.interval != undefined) {
+            let interval = c.specificData.interval;
+            console.log(now - (interval * 1000 * 60), lastExec);
+            if (lastExec == undefined || (interval != undefined && now - (interval * 1000 * 60) >= lastExec)) {
 
 
-
-              //this.http.globalAgent.maxSockets = 5000;
-              let keepAliveAgent = new this.http.Agent({
-                keepAlive: true
-              });
-
-              const parsedUrl = this.url.parse(this.config.timerTarget.target);
-              //console.log(parsedUrl);
-              this.http.get({
-                host: parsedUrl.hostname,
-                port: parsedUrl.port,
-                path: '/data/core/workspaceComponent/' + c._id + '/work',
-                headers: {
-                  "Authorization": "JTW" + " " + token
-                },
-                agent: keepAliveAgent
-              }, function(res) {
-                if (res.statusCode == 200) {
-
+              if (dedicaded && this.config.timerTarget != undefined) {
+                c.specificData.last = now;
+                //console.log(c);
+                this.componentLib.update(c);
+                const payload = {
+                  exp: this.moment().add(14, 'days').unix(),
+                  iat: this.moment().unix(),
+                  iss: 'timerScheduler'
                 }
-                res.on('end', () => {
-                  console.log('END Timer Work');
-                });
-              }.bind(this)).on('error', (e) => {
-                console.error('timer work request fail', e);
-                //throw new Error(e)
-              });
 
+                const token = this.jwt.encode(payload, this.config.timerTarget.secret);
+
+
+
+                //this.http.globalAgent.maxSockets = 5000;
+                let keepAliveAgent = new this.http.Agent({
+                  keepAlive: true
+                });
+
+                const parsedUrl = this.url.parse(this.config.timerTarget.target);
+                //console.log(parsedUrl);
+                this.http.get({
+                  host: parsedUrl.hostname,
+                  port: parsedUrl.port,
+                  path: '/data/core/workspaceComponent/' + c._id + '/work',
+                  headers: {
+                    "Authorization": "JTW" + " " + token
+                  },
+                  agent: keepAliveAgent
+                }, function(res) {
+                  if (res.statusCode == 200) {
+
+                  }
+                  res.on('end', () => {
+                    console.log('END Timer Work');
+                  });
+                }.bind(this)).on('error', (e) => {
+                  console.error('timer work request fail', e);
+                  //throw new Error(e)
+                });
+
+              } else if (dedicaded==undefined && this.config.timerTarget == undefined) {
+                let engine = require('../../webServices/recursivPullResolvePromise');
+                engine.getNewInstance().resolveComponent(c, 'work').then(() => {
+                  console.log('timer done');
+                })
+              }
             }
           }
-        }
+        });
+      }
 
 
 
-        // this.httpGet.makeRequest('GET', workUrl,undefined,{headers:{"Authorization": "JTW" + " " + token}}).then(result => {
-        // }).then(function(data) {
-        //   console.log("IN WORKSPACE COMPONENT RETURN DATA |", data)
-        //   //res.json(data.data);
-        // }).catch(e => {
-        //   console.log('Timer Service Run Error', e);
-        //   //next(e);
-        // });
 
-        //var recursivPullResolvePromiseDynamic = require('./webServices/recursivPullResolvePromise');
-        // recursivPullResolvePromiseDynamic.getNewInstance().resolveComponent(c, 'work').then(function(data) {
-        //   console.log("IN WORKSPACE COMPONENT RETURN DATA |", data)
-        //   //res.json(data.data);
-        // }).catch(e => {
-        //   console.log('Timer Service Run Error', e.displayMessage);
-        //   //next(e);
-        // });
-      })
+      // this.httpGet.makeRequest('GET', workUrl,undefined,{headers:{"Authorization": "JTW" + " " + token}}).then(result => {
+      // }).then(function(data) {
+      //   console.log("IN WORKSPACE COMPONENT RETURN DATA |", data)
+      //   //res.json(data.data);
+      // }).catch(e => {
+      //   console.log('Timer Service Run Error', e);
+      //   //next(e);
+      // });
+
+      //var recursivPullResolvePromiseDynamic = require('./webServices/recursivPullResolvePromise');
+      // recursivPullResolvePromiseDynamic.getNewInstance().resolveComponent(c, 'work').then(function(data) {
+      //   console.log("IN WORKSPACE COMPONENT RETURN DATA |", data)
+      //   //res.json(data.data);
+      // }).catch(e => {
+      //   console.log('Timer Service Run Error', e.displayMessage);
+      //   //next(e);
+      // });
+
     })
 
   },
