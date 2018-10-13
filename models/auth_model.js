d53e4b634957a0480a48ca814cedb35d26cbee4e
Gaëtan Rizio <gaetan+git@rizio.fr>
2018-10-13 22:56:49 +0200

    Add types for `auth_lib`
    To ease refactoring and add autocompletion in IDE,
    define types of parameters and results of functions.
    
    This commit does the following:
    
    - Add types in jsdoc for parameter functions
    - Declare model types in TS for complex syntax and IDE support

--- models/auth_model.js
+++ models/auth_model.js
@@ -3,4 +3,5 @@
 var mongoose = require('../db/mongo_client');
 var AuthenticationSchema = require('../model_schemas/auth_schema');
 
+/** @type module:mongoose.Model<AuthenticationDocument> */
 module.exports = mongoose.model('authentication', AuthenticationSchema);
--- models/user_model.js
+++ models/user_model.js
@@ -3,4 +3,5 @@
 var mongoose = require('../db/mongo_client');
 var userSchema = require('../model_schemas/user_schema');
 
+/** @type module:mongoose.Model<UserDocument> */
 module.exports = mongoose.model('User', userSchema);
